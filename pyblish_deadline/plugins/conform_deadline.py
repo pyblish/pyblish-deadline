ddef2664d35ae1ba797eb3771a10f93e4e896cb3
Toke Jepsen <tokejepsen@gmail.com>
2015-09-23 22:49:30 +0100

    fixes #23

--- pyblish_deadline/plugins/conform_deadline.py
+++ pyblish_deadline/plugins/conform_deadline.py
@@ -7,14 +7,7 @@ import inspect
 import sys
 
 import pyblish.api
-
-# adding vendor
-requests_path = os.path.dirname(inspect.getfile(inspect.currentframe()))
-requests_path = os.path.dirname(os.path.dirname(requests_path))
-requests_path = os.path.join(requests_path, 'vendor', 'requests')
-sys.path.append(requests_path)
-
-import requests
+from pyblish_deadline.vendor import requests
 
 
 class IntegrateDeadline(pyblish.api.Integrator):
@@ -113,27 +106,43 @@ class IntegrateDeadline(pyblish.api.Integrator):
         # submitting job
         args = [job_path, plugin_path]
 
+        # submitting remotely
+        success = True
+        try:
+            d = os.path.dirname
+            config = d(d(d(inspect.getfile(inspect.currentframe()))))
+            config = os.path.join(config, 'config.json')
+
+            data = ''
+            with open(config) as f:
+                data = json.load(f)
+                f.close()
+            url = '%s:%s/api/jobs' % (data['address'], data['port'])
+
+            payload = {'JobInfo': job_data, 'PluginInfo': plugin_data, 'AuxFiles': []}
+            r = requests.post(url, auth=(data['username'], data['password']), data=json.dumps(payload))
+
+            if r.status_code != 200:
+                success = False
+        except:
+            success = False
+        finally:
+            if success:
+                self.log.info('Successfully submitted to remote repository.')
+                return
+            else:
+                self.log.info('Failed to submit to remote repository.')
+
+
+         # submitting locally
         try:
             self.log.info(self.CallDeadlineCommand(args))
         except:
             self.log.warning('No local Deadline found!')
 
-            # deleting temporary files
-            os.remove(job_path)
-            os.remove(plugin_path)
-
-        d = os.path.dirname
-        config = d(d(d(inspect.getfile(inspect.currentframe()))))
-        config = os.path.join(config, 'config.json')
-
-        data = ''
-        with open(config) as f:
-            data = json.load(f)
-            f.close()
-        url = '%s:%s/api/jobs' % (data['address'], data['port'])
-
-        payload = {'JobInfo': job_data, 'PluginInfo': plugin_data, 'AuxFiles': []}
-        self.log.info(requests.post(url, auth=(data['username'], data['password']), data=json.dumps(payload)))
+        # deleting temporary files
+        os.remove(job_path)
+        os.remove(plugin_path)
 
     def CallDeadlineCommand(self, arguments, hideWindow=True):
         # On OSX, we look for the DEADLINE_PATH file. On other platforms, we use the environment variable.
